66841b2ddbdd2641813d211cf50e441b4465ba32
Brandon <pacewicbrandon@gmail.com>
2021-11-11 14:54:48 -0800

    progress save

--- SimonStoresCodev1.1/main.cpp
+++ SimonStoresCodev1.1/main.cpp
@@ -227,7 +227,7 @@ int main(){
                     for (int i = 0; i < 3; i++) {
                         if (flash[i] == 'r' || flash[i] == 'g' || flash[i] == 'b') {
                             temp[i] = local_stageTwo(flash[i], b[s-1], s, d, a);
-                            std::cout << temp[0] << ' ' << temp[1] << ' ' << temp[2]; endl;
+                            std::cout << temp[0] << ' ' << temp[1] << ' ' << temp[2] << temp[3] << ' ' << temp[4]; endl;
                         } else {
                             temp[4] = local_stageTwo(flash[i], a[s-1], s, d, a);
                         }
