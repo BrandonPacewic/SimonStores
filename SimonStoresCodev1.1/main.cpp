2ce82c966be9062c5d1061ddd1a1fa75cd4977e9
Brandon <pacewicbrandon@gmail.com>
2021-11-16 12:44:41 -0800

    update to dbg

--- SimonStoresCodev1.1/main.cpp
+++ SimonStoresCodev1.1/main.cpp
@@ -8,9 +8,15 @@ using namespace std;
 #include "balancedConverter.cpp"
 
 //dbg
-template<typename T> T printList(T in, const int si) { for (int i = 0; i < si; i++) { cout << in[i] << ' '; } return in; }
-template<typename T> T printReverse(T in, const int si) { for (int i = si - 1; i >= 0; i--) { cout << in[i]; } endl; return in; }
-template<typename funfun> funfun intDbg(funfun test, const int si) { cout << "{ "; printList(test, si); cout << " } \n"; return test; }
+long long dbg_count = 0ll;
+void dbg_out1() { endl; dbg_count += 1ll; }
+template<typename Front, typename... Back> void dbg_out1(Front K, Back... T) { cerr << ' ' << K; dbg_out1(T...); }
+#define testArgs(...) cerr << '#' << dbg_count << " __VA_ARGS__ (" << #__VA_ARGS__ << "):", dbg_out1(__VA_ARGS__)
+
+const string data_type[1] = { "St6vectorIiSaIiEE" };
+void assertListType() { cerr << '#' << dbg_count << " __LIST_ARGS__: ("; dbg_count += 1ll; }
+bool findType(string type) { for (auto i: data_type) { if (i == type) return true; } return false; }
+template<typename Arr> void testListInt(Arr List) { assert(findType(typeid(List).name())); assertListType(); for (int i: List) { cout << ' ' << i; } cerr << " )" << endl; }
 
 //error catch, m for max value r for required value
 template<typename N> N errorCatch(N testVal, const int maxSize, const char type) {
@@ -160,7 +166,6 @@ int main() {
         }
 
         cout << d << '\n';
-        intDbg(a, 4);
 
         local_colorSumbitingOrder(colorOrder, colorStageOne);
         local_ternaryConverter(a[3]);
