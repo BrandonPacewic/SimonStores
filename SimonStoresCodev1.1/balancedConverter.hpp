df3088e5ffdb52a7a97071178fe5b937a600355a
Brandon <pacewicbrandon@gmail.com>
2021-11-23 10:14:02 -0800

    finished seperating functions

--- SimonStoresCodev1.1/balancedConverter.hpp
+++ SimonStoresCodev1.1/balancedConverter.hpp
@@ -7,61 +7,115 @@ using namespace std;
 //max value that will need to be converted specifyed in the manual
 const int MAX_VAL = 364;
 
-template<typename T> T local_ternaryConverter(T conVal) {
-    int64_t ReConVal = int64_t(conVal);
-    bool neg = false;
-    if (ReConVal < 0) {
-        neg = true;
-        ReConVal *= int64_t(-1);
-    }
+// template<typename T> T local_ternaryConverter(T conVal) {
+//     int64_t ReConVal = int64_t(conVal);
+//     bool neg = false;
+//     if (ReConVal < 0) {
+//         neg = true;
+//         ReConVal *= int64_t(-1);
+//     }
 
-    vector<int64_t> vals;
-    for (int i = 0; pow(3, i) <= MAX_VAL; i++) 
-        vals.pb(int64_t(pow(3, i)));
+//     vector<int64_t> vals;
+//     for (int i = 0; pow(3, i) <= MAX_VAL; i++) 
+//         vals.pb(int64_t(pow(3, i)));
         
-    sort(vals.begin(), vals.end(), greater<int64_t>());
+//     sort(vals.begin(), vals.end(), greater<int64_t>());
 
-    vector<int> balTer(vals.size(), 0);
-    for (int i = 0; i < vals.size(); i++) 
-        for (int j = 0; j < 2; j++) 
-            if (ReConVal >= vals[i]) {
-                balTer[i]++;
-                ReConVal -= vals[i]; 
-            }
+//     vector<int> balTer(vals.size(), 0);
+//     for (int i = 0; i < vals.size(); i++) 
+//         for (int j = 0; j < 2; j++) 
+//             if (ReConVal >= vals[i]) {
+//                 balTer[i]++;
+//                 ReConVal -= vals[i]; 
+//             }
     
-    for (int i = balTer.size() - 1; i > 0; i--) {
-        if (balTer[i] == 3) {
-            balTer[i] = 0;
-            balTer[i-1]++;
-        } else if (balTer[i] == 2) {
-                balTer[i-1]++; 
-                balTer[i] = -1;
-        } 
-    }
+//     for (int i = balTer.size() - 1; i > 0; i--) {
+//         if (balTer[i] == 3) {template<typename T> T local_ternaryConverter(T conVal) {
+//     int64_t ReConVal = int64_t(conVal);
+//     bool neg = false;
+//     if (ReConVal < 0) {
+//         neg = true;
+//         ReConVal *= int64_t(-1);
+//     }
 
-    vector<char> ans(balTer.size(), '0');
-    for (int i = 0; i < balTer.size(); i++) {
-        if (balTer[i] == 1) {
-            ans[i] = '+';
-        } else if (balTer[i] == -1) 
-            ans[i] = '-';
-    }
-
-    if (neg)
-        for (auto &i: ans) {
-            if (i == '+') {
-                i = '-';
-            } else if (i == '-') {
-                i = '+';
-            }
-        }
+//     vector<int64_t> vals;
+//     for (int i = 0; pow(3, i) <= MAX_VAL; i++) 
+//         vals.pb(int64_t(pow(3, i)));
         
-    for (int i = ans.size() - 1; i >= 0; i--)
-        cout << ans[i];
-    endl;
+//     sort(vals.begin(), vals.end(), greater<int64_t>());
 
-    return conVal;
-}
+//     vector<int> balTer(vals.size(), 0);
+//     for (int i = 0; i < vals.size(); i++) 
+//         for (int j = 0; j < 2; j++) 
+//             if (ReConVal >= vals[i]) {
+//                 balTer[i]++;
+//                 ReConVal -= vals[i]; 
+//             }
+    
+//     for (int i = balTer.size() - 1; i > 0; i--) {
+//         if (balTer[i] == 3) {
+//             balTer[i] = 0;
+//             balTer[i-1]++;
+//         } else if (balTer[i] == 2) {
+//                 balTer[i-1]++; 
+//                 balTer[i] = -1;
+//         } 
+//     }
+
+//     vector<char> ans(balTer.size(), '0');
+//     for (int i = 0; i < balTer.size(); i++) {
+//         if (balTer[i] == 1) {
+//             ans[i] = '+';
+//         } else if (balTer[i] == -1) 
+//             ans[i] = '-';
+//     }
+
+//     if (neg)
+//         for (auto &i: ans) {
+//             if (i == '+') {
+//                 i = '-';
+//             } else if (i == '-') {
+//                 i = '+';
+//             }
+//         }
+        
+//     for (int i = ans.size() - 1; i >= 0; i--)
+//         cout << ans[i];
+//     endl;
+
+//     return conVal;
+// }
+//             balTer[i] = 0;
+//             balTer[i-1]++;
+//         } else if (balTer[i] == 2) {
+//                 balTer[i-1]++; 
+//                 balTer[i] = -1;
+//         } 
+//     }
+
+//     vector<char> ans(balTer.size(), '0');
+//     for (int i = 0; i < balTer.size(); i++) {
+//         if (balTer[i] == 1) {
+//             ans[i] = '+';
+//         } else if (balTer[i] == -1) 
+//             ans[i] = '-';
+//     }
+
+//     if (neg)
+//         for (auto &i: ans) {
+//             if (i == '+') {
+//                 i = '-';
+//             } else if (i == '-') {
+//                 i = '+';
+//             }
+//         }
+        
+//     for (int i = ans.size() - 1; i >= 0; i--)
+//         cout << ans[i];
+//     endl;
+
+//     return conVal;
+// }
 
 template<typename T_Num> 
 vector<int64_t> initalizeValues(T_Num ValueToBeConverted) {
@@ -77,14 +131,41 @@ vector<int64_t> initalizeValues(T_Num ValueToBeConverted) {
 }
 
 vector<int> makeTernaryArray(vector<int64_t> VALS, int64_t valueToBeConverted) {
-    vector<int> balTer(VALS.size(), 0);
+    vector<int> ternaryArray(VALS.size(), 0);
 
     for (int i = 0; i < VALS.size(); i++) { 
         for (int j = 0; j < 2; j++) { 
-            if (ReConVal >= VALS[i]) {
-                balTer[i]++;
-                ReConVal -= VALS[i]; 
+            if (valueToBeConverted >= VALS[i]) {
+                ternaryArray[i]++;
+                valueToBeConverted -= VALS[i]; 
             }
         }
     }
+
+    return ternaryArray;
+}
+
+void convertTernaryArrayToBalancedTernary(vector<int> &ternaryArray) {
+        for (int i = ternaryArray.size() - 1; i > 0; i--) {
+        if (ternaryArray[i] == 3) {
+            ternaryArray[i] = 0;
+            ternaryArray[i-1]++;
+        } else if (ternaryArray[i] == 2) {
+                ternaryArray[i-1]++; 
+                ternaryArray[i] = -1;
+        } 
+    }
+}
+
+vector<char> convertToSymbols(vector<int> balancedTernaryArray, bool invertSigns = false) {
+    vector<char> ans(balancedTernaryArray.size(), '0');
+    
+    for (int i = 0; i < balancedTernaryArray.size(); i++) {
+        if (balancedTernaryArray[i] == 1) {
+            ans[i] = invertSigns ? '-' : '+';
+        } else if (balancedTernaryArray[i] == -1) 
+            ans[i] = invertSigns ? '+' : '-';
+    }
+
+    return ans;
 }
