2b81d7cffa0577ba47ce72279f64046cd838f294
Brandon <pacewicbrandon@gmail.com>
2021-11-30 21:13:54 -0800

    changed bits to specific librarys and fixed abs included in the cmath library

--- SimonStoresCodev1.1/balancedTernaryConverter/balancedTernaryConverter.hpp
+++ SimonStoresCodev1.1/balancedTernaryConverter/balancedTernaryConverter.hpp
@@ -1,24 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
 
 template<typename T_Num, typename T_Vector>
 class BALANCED_TERNARY_CONVERTER {
 
 private: 
-    vector<int64_t> initalizeValues(T_Num ValueToBeConverted) {
-        vector<int64_t> VALUES;
+    template<typename T> T abs(T T_Type) { return T_Type < 0 ? T_Type *= -1 : T_Type; }
+
+    std::vector<int64_t> initalizeValues(T_Num ValueToBeConverted) {
+        std::vector<int64_t> VALUES;
 
         for (int i = 0; pow(3, i) <= ValueToBeConverted; i++) {
             VALUES.push_back(int64_t(pow(3, i)));
         }
-            
-        sort(VALUES.begin(), VALUES.end(), greater<int64_t>());
+
+        sort(VALUES.begin(), VALUES.end(), std::greater<int64_t>());
         
         return VALUES;
     }
 
-    vector<int> makeTernaryArray(const vector<int64_t> VALUES, int64_t valueToBeConverted) {
-        vector<int> ternaryArray(VALUES.size(), 0);
+    std::vector<int> makeTernaryArray(const std::vector<int64_t> VALUES, int64_t valueToBeConverted) {
+        std::vector<int> ternaryArray(VALUES.size(), 0);
 
         for (int i = 0; i < VALUES.size(); i++) { 
             for (int j = 0; j < 2; j++) { 
@@ -32,7 +36,7 @@ private:
         return ternaryArray;
     }
 
-    void convertTernaryArrayToBalancedTernary(vector<int> &ternaryArray) {
+    void convertTernaryArrayToBalancedTernary(std::vector<int> &ternaryArray) {
         for (int i = ternaryArray.size() - 1; i > 0; i--) {
             if (ternaryArray[i] == 3) {
                 ternaryArray[i] = 0;
@@ -44,8 +48,8 @@ private:
         }
     }
 
-    T_Vector convertToSymbols(const vector<int> &balancedTernaryArray, const bool invertSigns = false) {
-        vector<char> ans(balancedTernaryArray.size(), '0');
+    T_Vector convertToSymbols(const std::vector<int> &balancedTernaryArray, const bool invertSigns = false) {
+        std::vector<char> ans(balancedTernaryArray.size(), '0');
         
         for (int i = 0; i < balancedTernaryArray.size(); i++) {
             if (balancedTernaryArray[i] == 1) {
@@ -57,15 +61,16 @@ private:
         return ans;
     }
 
-    void printAnswer(vector<char> ans) {
+    void printAnswer(std::vector<char> ans) {
         for (auto i : ans) {
-            cout << i;
+            std::cout << i;
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 
 public:
-    const int MAX_VALUE = 364; //max int size to be converted found in the manual
+    //max int size to be converted found in the manual
+    const int MAX_VALUE = 364; 
 
     void convert(T_Num valueToBeConverted) {
         int64_t standardizedValueToBeConverted = int64_t(valueToBeConverted);
@@ -75,9 +80,9 @@ public:
             invertSigns = true;
         }
 
-        vector<int64_t> values = initalizeValues(MAX_VALUE);
+        std::vector<int64_t> values = initalizeValues(MAX_VALUE);
 
-        vector<int> ternaryArray = makeTernaryArray(values, standardizedValueToBeConverted);
+        std::vector<int> ternaryArray = makeTernaryArray(values, standardizedValueToBeConverted);
 
         convertTernaryArrayToBalancedTernary(ternaryArray);
 
