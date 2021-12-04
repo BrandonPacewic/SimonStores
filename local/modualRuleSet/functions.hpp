40da67fae928b76a06fc1f0e536aa8b4b3eda9d5
Brandon <pacewicbrandon@gmail.com>
2021-12-04 12:30:09 -0800

    changes to static functions

--- local/modualRuleSet/functions.hpp
+++ local/modualRuleSet/functions.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <cassert>
 
-class LOCAL_FUNCTIONS {
+class localFunctions {
 private:
     template<typename T> T abs(T num) {
         return num < 0 ? num *= -1 : num;
@@ -144,7 +144,6 @@ private:
     }
 
 public:
-
     static std::string getbaseColorOrder(const int stage) {
         const std::string baseColorOrderStageOne = "rgbcmy";
         const std::string baseColorCrderStageTwo = "ybgmcr";
