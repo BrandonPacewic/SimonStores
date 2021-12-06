318c2da6bf48d799f92131738bce7bcfc8496b31
Brandon <pacewicbrandon@gmail.com>
2021-12-06 09:48:28 -0800

    added inital calculation functions

--- local/modualRuleSet/functions.hpp
+++ local/modualRuleSet/functions.hpp
@@ -5,8 +5,10 @@
 #include <vector>
 #include <cassert>
 
-class localFunctions {
+class functions {
 private:
+    static const int modOpperator = 365;
+
     template<typename T> T abs(T num) {
         return num < 0 ? num *= -1 : num;
     }
@@ -141,6 +143,63 @@ private:
                 }
             }
         }
+    }
+
+    static int initalAlphaCalculation(const std::array<int, 6> serialBase36, const std::array<char, 6> numOrChar) {
+        int tempOne, tempTwo;
+
+        if (numOrChar[2] == 'i') {
+            tempOne = serialBase36[2] * 36;
+        } else {
+            tempOne = (serialBase36[2] + 9) * 36;
+        }
+
+        if (numOrChar[3] == 'i') {
+            tempTwo = serialBase36[3];
+        } else {
+            tempOne = serialBase36[3] + 9;
+        }
+        
+        tempOne %= modOpperator;
+        tempTwo %= modOpperator;
+
+        return tempOne + tempTwo;
+    }
+
+    static int initalBravoCalculation(const std::array<int, 6> serialBase36, const std::array<char, 6> numOrChar) {
+        int temp;
+
+        if (numOrChar[4] == 'i') {
+            temp = serialBase36[4] * 36;
+        } else {
+            temp = (serialBase36[4] + 9) * 36; 
+        }
+
+        temp %= modOpperator;
+
+        //serial index 5 is always a num
+        return temp + serialBase36[5];
+    }
+
+    static int initalCharlieCalculation(const std::array<int, 6> serialBase36, const std::array<char, 6> numOrChar) {
+        int tempOne, tempTwo;
+        
+        if (numOrChar[0] == 'i') {
+            tempOne = serialBase36[0] * 36;
+        } else {
+            tempOne = (serialBase36[0] + 9) * 36;
+        }
+
+        if (numOrChar[1] == 'c') {
+            tempTwo = serialBase36[1] + 9;
+        } else {
+            tempTwo = serialBase36[1];
+        }
+
+        tempOne %= modOpperator;
+        tempTwo %= modOpperator;
+
+        return tempOne + tempTwo;
     }
 
 public:
