3fe169a1d88c7cdebd95344477d3392f86d55e7f
Brandon <pacewicbrandon@gmail.com>
2021-12-05 23:08:03 -0800

    added class structure

--- userErrorCatch/errorCatch.hpp
+++ userErrorCatch/errorCatch.hpp
@@ -4,10 +4,35 @@
 class ErrorCatch {
 private:
 
-    static std::string checkForRequiredSize(std::string &testStr) {
-        ;
+    static void local_checkForRequiredSize(std::string &testStr, const int requiredSize) {
+        while (testStr.length() != requiredSize) {
+            std::cout << '\n' << '\n' << "You did not meet the required size of " << requiredSize << '.' << '\n' << "Please try again:";
+            getline(std::cin, testStr);
+        }
     }
 
-    static std::string checkForRequiredSize
+    static void local_checkForMaxSize(std::string &testStr, const int maxSize) {
+        while (testStr.length() > maxSize) {
+            std::cout << '\n' << '\n' << "You exceeded the maximum size of " << maxSize << '.' << '\n' << "please try again:";
+            getline(std::cin, testStr);
+        }
+    }
+
+public:
+
+    static std::string userStringInput(const std::string inputType, const int expectedLength) {
+        std::string userInputStr;
+        getline(std::cin, userInputStr);
+
+        if (inputType == "CheckForRequiredSize") {
+            local_checkForRequiredSize(userInputStr, expectedLength);
+        } else if (inputType == "CheckForMaxSize") {
+            local_checkForMaxSize(userInputStr, expectedLength);
+        } else {
+            assert(false);
+        }
+
+        return userInputStr;
+    }
 
 };
