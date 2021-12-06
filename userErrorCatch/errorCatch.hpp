4510bed8cdb28c272630f8880667dd01ac7dbc5d
Brandon <pacewicbrandon@gmail.com>
2021-12-05 22:50:51 -0800

    inital changes

---
+++ userErrorCatch/errorCatch.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cassert>
+
+class ErrorCatch {
+private:
+
+    static std::string checkForRequiredSize(std::string &testStr) {
+        ;
+    }
+
+    static std::string checkForRequiredSize
+
+};
