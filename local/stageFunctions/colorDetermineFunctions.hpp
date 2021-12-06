9a6b1a68b632b8ee46a2cfcf0f7f63d66246fb69
Brandon <pacewicbrandon@gmail.com>
2021-12-06 14:34:38 -0800

    file should be done

--- local/stageFunctions/colorDetermineFunctions.hpp
+++ local/stageFunctions/colorDetermineFunctions.hpp
@@ -1,21 +1,91 @@
 #include <string>
-#include <array>
 #include <vector>
 
 namespace colorDetermine {
-    int addColorMix(std::string &flash) {
-        std::vector<int> temp(3, 0);
 
-        for (auto &i: flash)
-            i = tolower(i);
+    int primaryVsSecondaryMix(std::string &flash) {
+        std::vector<int> temp(3, 0);
         
         for (int i = flash.length(); i > 0; i--) {
-            if (flash[i-1] == 'r' || flash[i-1] == 'g' || flash[i-1] == 'b') {
+            if (tolower(flash[i-1]) == 'r' || tolower(flash[i-1]) == 'g' || tolower(flash[i-1]) == 'b') {
                 temp[i-1] = 1;
             } else {
                 temp[i-1] = 2;
             }
         }
-        return(temp[0] + temp[1] + temp[2]);
+
+        return temp[0] + temp[1] + temp[2];
+    }
+
+    char missingColor(const std::string flash) {
+        int x = 0;
+        bool primaryColors = false;
+
+        for (auto ch : flash) {
+            switch (tolower(ch)) {
+                case 'r':
+                    x = x + 1;
+                    primaryColors = true;
+                    break;
+
+                case 'g':
+                    x = x + 2;
+                    primaryColors = true;
+                    break;
+
+                case 'b':
+                    x = x + 3;
+                    primaryColors = true;
+                    break;
+
+                case 'c':
+                    x = x + 1;
+                    break;
+
+                case 'm':
+                    x = x + 2;
+                    break;
+
+                case 'y':
+                    x = x + 3;
+                    break;            
+            }
+        }
+
+        if (primaryColors) {
+            switch (x) {
+                case 5:
+                    return('r');
+                    break;
+
+                case 4:
+                    return('g');
+                    break;
+
+                case 3:
+                    return('b');
+                    break;
+                default:
+                    return 'X';
+            }
+
+        } else {
+            switch (x) {
+                case 5:
+                    return('c');
+                    break;
+
+                case 4:
+                    return('m');
+                    break;
+
+                case 3:
+                    return('y');
+                    break;
+                
+                default:
+                    return 'X';
+            }
+        }
     }
 }
