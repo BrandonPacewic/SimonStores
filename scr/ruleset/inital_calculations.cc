001be94c4ad919d66e9df3d19242b01f04537b55
Brandon <pacewicbrandon@gmail.com>
2022-03-15 21:57:02 -0700

    added inital caluclation files

---
+++ scr/ruleset/inital_calculations.cc
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Brandon Pacewic
+ *
+ * Developed and tested by Brandon Pacewic
+ * 
+ * inital_calculations.cc
+ */
+
+#ifndef _INITAL_CALCULATIONS_C
+#define _INITAL_CALCULATIONS_C 1
+
+
+
+#endif // _INITAL_CALCULATIONS_C
---
+++ scr/ruleset/inital_calculations.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 Brandon Pacewic
+ *
+ * Developed and tested by Brandon Pacewic
+ * 
+ * inital_calculations.h
+ */
+
+#pragma once
+#ifndef _INITAL_CALCULATIONS_H
+#define _INITAL_CALCULATIONS_H 1
+
+namespace inital_calculations {
+
+int alpha() {
+    // TODO
+    return 0;
+}
+
+} // namespace inital_calculations
+
+#endif // _INITAL_CALCULATIONS_H
