64f791bbf734eb45b36b63e2a22c4dfcb7a93d5a
Brandon <pacewicbrandon@gmail.com>
2022-03-16 11:49:53 -0700

    done with color determine functions

---
+++ scr/ruleset/color_determine.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 Brandon Pacewic
+ *
+ * Developed and tested by Brandon Pacewic
+ * 
+ * color_determine.h
+ */
+
+#pragma once
+#ifndef _COLOR_DETERMINE_H
+#define _COLOR_DETERMINE_H 1
+
+#include <string>
+
+namespace color_determine {
+
+int primary_secondary_mix(const std::string& flash);
+char missing_color(const std::string& flash);
+
+} // namespace color_determine
+
+#endif // _COLOR_DETERMINE_H
