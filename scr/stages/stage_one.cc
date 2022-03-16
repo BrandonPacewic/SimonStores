09c2415ba8e15ddd767c820833969d338d0a7eaf
Brandon <pacewicbrandon@gmail.com>
2022-03-16 13:57:11 -0700

    done with stage one calculations

--- scr/stages/stage_one.cc
+++ scr/stages/stage_one.cc
@@ -9,6 +9,8 @@
 #ifndef _STAGE_ONE_C
 #define _STAGE_ONE_C 1
 
+#include <algorithm>
+#include <array>
 #include <cassert>
 #include <cmath>
 #include <functional>
@@ -16,7 +18,9 @@
 #include <vector>
 
 #include "../math/static_mod_type.h"
+#include "../ruleset/color_determine.h"
 using mod_types::static_mod_type;
+using color_determine::primary_secondary_mix;
 
 #include "stage_one.h"
 
@@ -74,13 +78,95 @@ int two_color_flash(const std::string& flash, const std::vector<int>& alpha,
     const int& step, const int& delta) {
     assert(flash.length() == 2);
 
-    //TODO: add color determine functions
-    // const int color_mix_value
+    const int color_mix_value = primary_secondary_mix(flash);
 
-    return 0;
+    const int first = color_to_function_map.find(flash[0])->second(
+        alpha[step-1], step, delta
+    );
+    const int second = color_to_function_map.find(flash[1])->second(
+        alpha[step-1], step, delta
+    );
+
+    static_mod_type<int> answer(mod_limit);
+
+    switch (color_mix_value) {
+        case 2:
+            answer += std::max(first, second);
+            break;
+        case 3:
+            answer += first + second - (delta * 2);
+            break;
+        case 4:
+            answer += std::min(first, second);
+            break;
+    }
+
+    return int(answer);
+}
+
+int three_color_flash(const std::string& flash, const std::vector<int>& alpha,
+    const int& step, const int& delta) {
+    assert(flash.length() == 3);
+
+    int color_mix_value = primary_secondary_mix(flash);
+    static_mod_type<int> answer(mod_limit);
+
+    if (color_mix_value == 3) {
+        answer += alpha[step-1] + alpha[0];
+    }
+    else if (color_mix_value == 4 || color_mix_value == 5) {
+        std::array<int, 3> color_calculations;
+
+        for (int i = 0; i < 3; ++i) {
+            color_calculations[i] = (
+                color_to_function_map.find(flash[i])->second(
+                    alpha[step-1], step, delta
+                )
+            );
+        }
+
+        switch (color_mix_value) {
+            case 4:
+                answer += *std::max_element(
+                    color_calculations.begin(), color_calculations.end()
+                );
+                break;
+            case 5:
+                answer += *std::min_element(
+                    color_calculations.begin(), color_calculations.end()
+                );
+                break;
+        }
+    }
+    else {
+        answer += alpha[step-1] - alpha[0];
+    }
+
+    return int(answer);
 }
 
 } // namespace
+
+int one_calculations(const std::string& flash, 
+    const std::vector<int>& alpha, const int& step, const int& delta) {
+    assert(flash.length() <= 3);
+
+    int answer = -999;
+
+    switch (flash.length()) {
+        case 1:
+            answer = one_color_flash(flash, alpha, step, delta);
+            break;
+        case 2:
+            answer = two_color_flash(flash, alpha, step, delta);
+            break;
+        case 3:
+            answer = three_color_flash(flash, alpha, step, delta);
+            break;
+    }
+
+    return answer;
+}
 
 } // namespace stages
 
