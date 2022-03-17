6a2a79b3a9a100a35ad3aab979ba743b8b29855d
Brandon <pacewicbrandon@gmail.com>
2022-03-17 14:33:48 -0700

    done with stage three two color calculations

--- scr/stages/stage_three.cc
+++ scr/stages/stage_three.cc
@@ -9,6 +9,10 @@
 #ifndef _STAGE_THREE_C
 #define _STAGE_THREE_C 1
 
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
 #include <functional>
 #include <string>
 #include <unordered_map>
@@ -19,6 +23,9 @@ using mod_types::mod_type;
 
 #include "../math/static_mod_type.h"
 using mod_types::static_mod_type;
+
+#include "../ruleset/color_determine.h"
+using color_determine::primary_secondary_mix;
 
 namespace stages {
 
@@ -31,7 +38,7 @@ const std::unordered_map<
         std::vector<mod_type<int>>, std::vector<mod_type<int>>, int, int, int
     )>
 > color_to_function_map = {
-    {'r', [](const std::vector<mod_type<int>>& a , 
+    {'r', [](const std::vector<mod_type<int>>& a, 
         const std::vector<mod_type<int>>& b, 
         const int& x, const int& s, const int& d) -> int {
         static_mod_type<int> answer(mod_limit);
@@ -75,6 +82,76 @@ const std::unordered_map<
     }},
 };
 
+int one_color_flash(const std::string& flash, 
+    const std::vector<mod_type<int>>& alpha, 
+    const std::vector<mod_type<int>>& bravo,
+    const std::vector<mod_type<int>>& charlie,
+    const int& step, const int& delta) {
+    assert(flash.length() == 1);
+
+    static_mod_type<int> answer(mod_limit);
+    answer += color_to_function_map.find(flash[0])->second(
+        alpha, bravo, int(charlie[step-1]), step, delta
+    );
+
+    return int(answer);
+}
+
+int two_color_flash(const std::string& flash,
+    const std::vector<mod_type<int>>& alpha,
+    const std::vector<mod_type<int>>& bravo,
+    const std::vector<mod_type<int>>& charlie,
+    const int& step, const int& delta) {
+    assert(flash.length() == 2);
+
+    const int color_mix_value = primary_secondary_mix(flash);
+    static_mod_type<int> answer(mod_limit);
+
+    if (color_mix_value == 2) {
+        const char absent_color = color_determine::missing_color(flash);
+
+        answer += (
+            color_to_function_map.find(absent_color)->second(
+                alpha, bravo, int(charlie[step-1]), step, delta
+            ) + (
+                color_to_function_map.find(absent_color)->second(
+                    alpha, bravo, int(bravo[step-1]), step, delta
+            )) + (
+                color_to_function_map.find(absent_color)->second(
+                    alpha, bravo, int(alpha[step-1]), step, delta
+            ))
+        );
+    }
+    else if (color_mix_value == 3) {
+        std::array<int, 3> color_values;
+        color_values[0] = color_to_function_map.find(flash[0])->second(
+            alpha, bravo, int(charlie[step-1]), step, delta
+        );
+        color_values[1] = color_to_function_map.find(flash[1])->second(
+            alpha, bravo, int(charlie[step-1]), step, delta
+        );
+        color_values[2] = -1 * abs(color_values[0] - color_values[1]);
+
+        answer += *std::min_element(color_values.begin(), color_values.end());
+    }
+    else {
+        const char absent_color = color_determine::missing_color(flash);
+
+        answer += int(
+            color_to_function_map.find(absent_color)->second(
+                alpha, bravo, int(charlie[step-1]), step, delta
+            ) - (
+                color_to_function_map.find(flash[0])->second(
+                    alpha, bravo, int(charlie[step-1]), step, delta
+            )) - (
+                color_to_function_map.find(flash[1])->second(
+                    alpha, bravo, int(charlie[step-1]), step, delta
+            ))
+        );
+    }
+
+    return int(answer);
+}
 
 
 } // namespace
