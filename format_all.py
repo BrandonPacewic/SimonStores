eedc03992be387b2aedc890050040204a28e990a
Brandon <pacewicbrandon@gmail.com>
2022-04-30 23:12:01 -0700

    Update to python script (#31)

--- format_all.py
+++ format_all.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    for dirpath, _, filenames in os.walk('src/'):
+    for dirpath, _, filenames in os.walk('.'):
         for filename in filenames:
             if filename.endswith('.cc'):
                 os.system(f'clang-format -i -style=file {dirpath}/{filename}')
