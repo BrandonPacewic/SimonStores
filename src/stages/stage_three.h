0384608b5eb2834578ed3cfb2a7d5d76f25679bc
Brandon <pacewicbrandon@gmail.com>
2022-05-03 14:41:24 -0700

    Update to stage three (#30)

--- src/stages/stage_three.h
+++ src/stages/stage_three.h
@@ -7,23 +7,24 @@
  */
 
 #pragma once
-#ifndef _STAGE_THREE_H
-#define _STAGE_THREE_H 1
+#ifndef SIMON_STORES_STAGES_STAGE_THREE_H_
+#define SIMON_STORES_STAGES_STAGE_THREE_H_
 
 #include <string>
 #include <vector>
 
 #include "../math/mod_type.h"
+
 using mod_types::mod_type;
 
 namespace stages {
 
-int three_calculations(const std::string& flash,
-                       const std::vector<mod_type<int>>& alpha,
-                       const std::vector<mod_type<int>>& bravo,
-                       const std::vector<mod_type<int>>& charlie,
-                       const int& step, const int& delta);
+int16_t three_calculations(const std::string& flash,
+                           const std::vector<mod_type<int16_t>>& alpha,
+                           const std::vector<mod_type<int16_t>>& bravo,
+                           const std::vector<mod_type<int16_t>>& charlie,
+                           const uint16_t& step, const int16_t& delta);
 
 }  // namespace stages
 
-#endif  // _STAGE_THREE_H
+#endif  // SIMON_STORES_STAGES_STAGE_THREE_H_
